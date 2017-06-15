aee468dd55ce059121f0422c55b5f1dcec30089d
mfergu <mfergu3@g.clemson.edu>
2017-06-15 12:12:03 -0400

    added some metadata still blank functions

--- plock.c
+++ plock.c
@@ -1,9 +1,28 @@
 #include "plock.h"
 
 typedef struct plock_t T;
+typedef struct node_t N;
+
+/*
+typedef struct{
+  int value;          // FREE or BUSY                            
+  pthread_mutex_t mlock;  // mutex protecting the plock structure    
+  node_t *head;       // linked list of waiting threads          
+} plock_t;
+
+typedef struct node_def{
+  int priority;       // priority of the waiting thread          
+  pthread_cond_t waitCV;  // thread waits on this condition variable 
+  struct node_def *next;  // node is part of a singly-linked list    
+} node_t;
+
+
+*/
 
 T *plock_create(){
-	T* temp;
+	T* temp = malloc(T);
+	temp->value = FREE;	
+	temp->mlock = PTHREAD_MUTEX_INITIALIZER;
 	return temp;
 }
 
