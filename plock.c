75e4f19a1d9a0f6f8254a2fce820687acae04ea3
mfergu <mfergu3@g.clemson.edu>
2017-06-15 09:54:53 -0400

    initial commit

---
+++ plock.c
@@ -0,0 +1,20 @@
+#include "plock.h"
+
+typedef struct plock_t T;
+
+T *plock_create(){
+	T* temp;
+	return temp;
+}
+
+void plock_destroy( T *lock ){
+
+}
+
+void plock_enter( T *lock, int priority ){
+
+}
+
+void plock_exit( T *lock ){
+
+}
